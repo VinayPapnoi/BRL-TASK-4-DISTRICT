e28856a863578b3a2361cfa8f5f15b8e5f3056ca
VinayPapnoi <papnoi.vinay@gmail.com>
2025-10-19 13:51:22 +0530

    added splash screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Wait 3 seconds, then go to home screen
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/home');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(55, 26, 166,255),// Purple
+      body: Center(
+        child: Image.asset(
+          'assets/images/district_logo.png',
+          width: 250,
+        ),
+      ),
+    );
+  }
+}
